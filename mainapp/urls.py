27ec7719eb447e3fb4b4d2eadee0fe605021eda0
Anna_Lebedeva <47499676+Anna-Lebedeva@users.noreply.github.com>
2021-03-02 15:55:21 -0300

    rm master

--- mainapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import *
-
-urlpatterns = [
-    path('', BaseView.as_view(), name='index'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('profile/', ProfileView.as_view(), name='profile'),
-]
